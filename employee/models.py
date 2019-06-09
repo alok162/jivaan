d27be114791f280daea8b55608bfec1b037df100
Alok Chodhary <alok.choudhary@moonraft.com>
2019-06-09 18:09:17 +0530

    create model

--- employee/models.py
+++ employee/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
+class Employee(models.Model):
+    employee_code = models.CharField(primary_key=True, max_length=30)
+    department = models.CharField(max_length=100)
+    score = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
